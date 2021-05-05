432664f88d44e16159b69e4ac686b9115671f31e
ajlevoy <ajlevoy86@gmail.com>
2021-05-05 13:16:38 -0700

    js additions, added 404

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,9 @@ const darkTee = document.getElementById('dark-tee');
 
 // image urls
 const lightGreyTeeUrl = "assets/images/vitalik-buterin-ethereum-t-shirt-light-grey.jpg";
-// const lightGreyTeeUrl = "{{'/assets/images/vitalik-buterin-ethereum-t-shirt-light-grey.jpg' | relative_url }}";
 const lightBlueTeeUrl = "assets/images/vitalik-buterin-ethereum-t-shirt-light-blue.jpg";
-// const lightBlueTeeUrl = "{{'/assets/images/vitalik-buterin-ethereum-t-shirt-light-blue.jpg' | relative_url }}";
-const darkGreyTeeUrl = "{{'/assets/images/vitalik-buterin-ethereum-t-shirt-dark-grey.jpg' | relative_url }}";
-const darkBlueTeeUrl = "{{'/assets/images/vitalik-buterin-ethereum-t-shirt-dark-grey.jpg' | relative_url }}";
+const darkGreyTeeUrl = "assets/images/vitalik-buterin-ethereum-t-shirt-dark-grey.jpg";
+const darkBlueTeeUrl = "assets/images/vitalik-buterin-ethereum-t-shirt-dark-blue.jpg";
 
 // swatches
 const swatchLightBlue = document.getElementById('swatch-light-blue');
@@ -16,12 +14,30 @@ const swatchLightGrey = document.getElementById('swatch-light-grey');
 const swatchDarkBlue = document.getElementById('swatch-dark-blue');
 const swatchDarkGrey = document.getElementById('swatch-dark-grey');
 
-// change light tee to blue
+// change light tee to blue, +/- swatch select
 swatchLightBlue.addEventListener('click', () => {
     lightTee.src = lightBlueTeeUrl;
+    swatchLightBlue.classList.add("swatch-select");
+    swatchLightGrey.classList.remove("swatch-select");
 });
 
-// change light tee to grey
+// change light tee to grey, +/- swatch select
 swatchLightGrey.addEventListener('click', () => {
     lightTee.src = lightGreyTeeUrl;
+    swatchLightGrey.classList.add("swatch-select");
+    swatchLightBlue.classList.remove("swatch-select");
+});
+
+// change dark tee to grey, +/- swatch select
+swatchDarkGrey.addEventListener('click', () => {
+    darkTee.src = darkGreyTeeUrl;
+    swatchDarkGrey.classList.add("swatch-select");
+    swatchDarkBlue.classList.remove("swatch-select");
+});
+
+// change dark tee to blue, +/- swatch select
+swatchDarkBlue.addEventListener('click', () => {
+    darkTee.src = darkBlueTeeUrl;
+    swatchDarkBlue.classList.add("swatch-select");
+    swatchDarkGrey.classList.remove("swatch-select");
 });
